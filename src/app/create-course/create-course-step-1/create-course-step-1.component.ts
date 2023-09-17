8171afe46d371daa7058be6fd46adb983aeae520
iTzMiNOS <amirerf1998@gmail.com>
2023-09-17 13:23:52 +0300

    modified:   src/app/create-course/create-course-step-1/create-course-step-1.component.html 	modified:   src/app/create-course/create-course-step-1/create-course-step-1.component.scss 	modified:   src/app/create-course/create-course-step-1/create-course-step-1.component.ts 	modified:   src/app/create-course/create-course.component.html 	modified:   src/app/home/home.component.html 	modified:   src/styles.scss

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,13 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UntypedFormBuilder, Validators} from '@angular/forms';
-
+import {MatCalendarCellClassFunction, MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: "create-course-step-1",
   templateUrl:"create-course-step-1.component.html",
   styleUrls: ["create-course-step-1.component.scss"]
 })
-export class CreateCourseStep1Component {
+export class CreateCourseStep1Component implements OnInit {
 
   form = this.fb.group({
     title: ['', [
@@ -16,14 +16,36 @@ export class CreateCourseStep1Component {
       Validators.maxLength(60)
     ]],
     releasedAt: [new Date(), Validators.required],
-    category: ['BEGINNER', Validators.required],
+    category: [[], Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   });
 
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+
+    const date = cellDate.getDate();
+
+    if (view === 'month') {
+      return (date === 1) || (date === 15) ? 'highlight-date': '';
+    }
+    return "";
+  }
+  myFilter = (d: Date | null): boolean => {
+    if (!d) {
+      return false;
+    }
+    const dayOfMonth = d.getDate();
+    // Prevent Saturday and Sunday from being selected.
+    return dayOfMonth !== 1 && dayOfMonth !== 15;
+  };
+
   constructor(private fb: UntypedFormBuilder) {
 
+  }
+  ReadOnlyStyleGuideNotes: boolean;
+  ngOnInit(): void {
+    this.ReadOnlyStyleGuideNotes = true;
   }
 
   get courseTitle() {
