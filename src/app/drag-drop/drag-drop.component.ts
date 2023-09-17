5c7fe0466817d3194da8c25e1a9ca85d3b916263
iTzMiNOS <amirerf1998@gmail.com>
2023-09-17 19:03:06 +0300

    modified:   src/app/app.component.html 	modified:   src/app/course-dialog/course-dialog.component.html 	modified:   src/app/course-dialog/course-dialog.component.ts 	modified:   src/app/courses-card-list/courses-card-list.component.ts 	modified:   src/app/drag-drop/drag-drop.component.html 	modified:   src/app/drag-drop/drag-drop.component.scss 	modified:   src/app/drag-drop/drag-drop.component.ts

--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -90,4 +90,29 @@ export class DragDropComponent {
     }
   ];
 
+  done = [];
+
+  dropMultiList(event: CdkDragDrop<Lesson[]>){
+
+    if(event.container == event.previousContainer){
+      moveItemInArray(event.container.data,event.previousIndex,event.currentIndex);
+    }else{
+
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+
+    }
+
+  }
+
+  drop(event: CdkDragDrop<Lesson[]>){
+
+    moveItemInArray(this.lessons,event.previousIndex,event.currentIndex);
+
+  }
+
 }
